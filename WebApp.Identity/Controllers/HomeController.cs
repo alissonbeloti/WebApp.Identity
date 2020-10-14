2272a7d09043c60df9c5938c2b545ae2a2fe2b5b
alissonbeloti <alisson.beloti@gmail.com>
2020-10-14 17:54:42 -0300

    Adicionar arquivos de projeto.

---
+++ WebApp.Identity/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity.Controllers
+{
+    //[ApiController]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await this.userManager.FindByNameAsync(model.UserName);
+                if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var identity = new ClaimsIdentity("cookies");
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+                    await HttpContext.SignInAsync("cookies", new ClaimsPrincipal(identity));
+                    return RedirectToAction("About");
+                }
+
+                ModelState.AddModelError("", "Usuário ou Senha inválida!");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public  IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public  IActionResult Success()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await this.userManager.FindByNameAsync(model.UserName);
+                if (user == null)
+                {
+                    user = new IdentityUser()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName,
+                    };
+
+                    var result = await userManager.CreateAsync(user, model.Password);
+                    if (result.Errors.Any())
+                    {
+                        return View();
+                    }
+                }
+                return View("Success");
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp.Identity/MyUserStore.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Dapper;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Identity
+{
+    public class MyUserStore : IUserStore<MyUser>, IUserPasswordStore<MyUser>
+    {
+        public async Task<IdentityResult> CreateAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync(@"INSERT INTO [dbo].[Users] ([Id] ,[UserName], [NormalizedUserName], [PasswordHash])
+     VALUES
+           (@Id, @UserName, @NormalizedUserName, @PasswordHash)", user);
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync("Delete from Users where id = @id", new { id = user.Id });
+            }
+            return IdentityResult.Success;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public static DbConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=IdentityCurso;Data Source=DESKTOP-ROLELGP\SQLEXPRESS");
+            connection.Open();
+            return connection;
+        }
+
+        public async Task<MyUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<MyUser>("Select * from Users where id = @id", new { id = userId });
+            }
+        }
+
+        public async Task<MyUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<MyUser>("Select * from Users where normalizedUserName = @name", new { name = normalizedUserName });
+            }
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.NormalizedUserName);
+        }
+
+        public Task<string> GetUserIdAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName);
+        }
+
+        public Task SetNormalizedUserNameAsync(MyUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            user.NormalizedUserName = normalizedName;
+            return Task.CompletedTask;
+        }
+
+        public Task SetUserNameAsync(MyUser user, string userName, CancellationToken cancellationToken)
+        {
+            user.UserName = userName;
+            return Task.CompletedTask;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            using (var connection = GetOpenConnection())
+            {
+                await connection.ExecuteAsync(@"UPDATE [dbo].[Users] SET [UserName] = @UserName ,[NormalizedUserName] = @NormalizedUserName, 
+                    [PasswordHash] = @PasswordHash
+                WHERE [Id] = @Id", user);
+            }
+            return IdentityResult.Success;
+        }
+
+        public Task SetPasswordHashAsync(MyUser user, string passwordHash, CancellationToken cancellationToken)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetPasswordHashAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(MyUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+    }
+}
